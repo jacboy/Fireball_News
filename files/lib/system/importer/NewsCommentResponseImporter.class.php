30b53144519c5b51b8cbc62a380df08a16a4ed3e
Jens Krumsieck <info@codequake.de>
2014-06-30 15:11:08 +0200

    first steps to switch news into own repo

---
+++ files/lib/system/importer/NewsCommentResponseImporter.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace cms\system\importer;
+
+use wcf\system\importer\AbstractCommentResponseImporter;
+
+class NewsCommentResponseImporter extends AbstractCommentResponseImporter {
+	protected $objectTypeName = 'de.codequake.cms.news.comment';
+}
